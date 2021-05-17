39b83cec89af6d070629622db0c83a8dcbb5877e
JDevanand <devanand.jayakumar@gmail.com>
2021-05-17 08:45:10 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Fragment>
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span>     
+          <span>Phone</span>
+        </div>
+      </Fragment>
     );
   }
+}
 
-//export default App; // this was present when function App was present
+export default App;
